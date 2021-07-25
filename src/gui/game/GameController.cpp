43a2137488dc0d7a87b5b47bb7ab55acc58a197c
krawthekrow <krawthekrow@gmail.com>
2021-07-25 01:10:55 -0400

    add stack edit

--- src/gui/game/GameController.cpp
+++ src/gui/game/GameController.cpp
@@ -1206,6 +1206,16 @@ ConfigTool * GameController::GetActiveConfigTool()
 	else return NULL;
 }
 
+int GameController::GetStackEditDepth()
+{
+	return gameModel->GetSimulation()->stackEditDepth;
+}
+
+void GameController::SetStackEditDepth(int depth)
+{
+	gameModel->GetSimulation()->stackEditDepth = depth;
+}
+
 int GameController::GetReplaceModeFlags()
 {
 	return gameModel->GetSimulation()->replaceModeFlags;
