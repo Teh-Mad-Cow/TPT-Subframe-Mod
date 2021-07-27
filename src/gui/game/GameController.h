62a0acf3185533acd76bbdc260c2adee4c217365
krawthekrow <krawthekrow@gmail.com>
2021-07-27 00:51:26 -0400

    show particle debug position in the hud

--- src/gui/game/GameController.h
+++ src/gui/game/GameController.h
@@ -126,6 +126,7 @@ public:
 	void SetActiveTool(int toolSelection, ByteString identifier);
 	void SetLastTool(Tool * tool);
 	SimulationSample * GetSample();
+	int GetParticleDebugPosition();
 	int GetReplaceModeFlags();
 	void SetReplaceModeFlags(int flags);
 	void SetActiveColourPreset(int preset);
